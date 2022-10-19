b4e4f336d34f314cbc30c7b24b44ad34f47ae8b3
Shezad <shezadhassan22@gmail.com>
2022-10-19 16:37:15 +0800

    Smoke test

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.16;
+
+contract Election {
+    
+    string public candidate;
+    // Constructor
+    constructor () {
+        candidate = "SHEZ";
+    }
+}

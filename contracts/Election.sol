f0165e03825771d8176bb64dca29fc3d9cd98942
Shezad <shezadhassan22@gmail.com>
2022-10-20 09:37:02 +0800

    Set up client side basics

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -3,9 +3,24 @@ pragma solidity >=0.5.16;
 
 contract Election {
     
-    string public candidate;
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Hashmap to track candidates
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
+
     // Constructor
-    constructor () {
-        candidate = "SHEZ";
+    constructor () public{
+        addCandidate("foo");
+        addCandidate("faa");
+    }
+    // _ means local variable not global variable
+    function addCandidate (string memory name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, name, 0);
     }
 }
